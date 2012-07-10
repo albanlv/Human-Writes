061aa364610d208e840c15c55226058fdc804cbb
Mu <julien.muresianu@gmail.com>
2012-07-10 08:29:25 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HumanWrites::Application.config.secret_token = '33363e89cd34568628aff9bf05610d5e29e2e1123320160cc0550d55538dc9c66152423dfb128204ded08fca62862d34b45ddcfc7623c7715df73cadd7192faa'
